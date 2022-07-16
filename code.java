b2f636ffecc4973006c1b73ad3c149e1553e123e
Papu <Papu1814@gmail.com>
2022-07-16 13:39:10 +0000

    5th Commit

--- code.java
+++ code.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("paapa main aapase pyaar karata hoon");  
+     System.out.println("papu main aapase pyaar karata hoon");  
     }  
 }  
