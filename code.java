f4c0f94998bbb8827ea5389dc02e419cc128d559
Pandss <Pandeeswari1814@gmail.com>
2022-07-16 14:36:23 +0000

    7th Commit

--- code.java
+++ code.java
@@ -1,6 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("papu MAIN  aapase pyaar karata hoon");
-     System.out.println("Papu");  
+     System.out.println("PAPU main aapase pyaar karata hoon");
     }  
 }  
